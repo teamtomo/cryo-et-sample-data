ce7e059ff76fee4201d57738fefbc2da008ddf38
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-12-10 09:27:27 +0100

    fix import

--- src/cryo_et_sample_data/__init__.py
+++ src/cryo_et_sample_data/__init__.py
@@ -2,6 +2,7 @@ try:
     from ._version import version as __version__
 except ImportError:
     __version__ = "unknown"
+from ._hiv import hiv
 from ._sample_data import make_sample_data
 
-__all__ = ("make_sample_data",)
+__all__ = ("make_sample_data", "hiv")
