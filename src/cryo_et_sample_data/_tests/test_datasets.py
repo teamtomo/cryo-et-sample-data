cec645a47797b0d12d1dd4255a519efbb23b784e
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2022-12-10 09:24:49 +0100

    make datasets importable from root

--- src/cryo_et_sample_data/_tests/test_datasets.py
+++ src/cryo_et_sample_data/_tests/test_datasets.py
@@ -1,6 +1,8 @@
-from cryo_et_sample_data._hiv import HIV
+from cryo_et_sample_data import hiv
+from cryo_et_sample_data._base import DataSet
 
 
 def test_hiv():
-    hiv = HIV()
-    print(hiv.tomogram)
+    assert hiv.name == "hiv"
+    assert hiv.author == "Alister Burt"
+    assert isinstance(hiv, DataSet)
